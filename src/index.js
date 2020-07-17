4e7c8a14312312b8f9515140799bd0d789af8b76
gabdulkhanov <gabdulkhanov.lenar@mail.ru>
2020-07-17 17:46:00 +0500

    lesson 35 observer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import { rerenderEntire } from './render';
-import state from './redux/state';
+import './index.css';
+import App from './App';
+import state, { subscibe, addPost, updateNewPost } from './redux/state';
 
 
-rerenderEntire(state);
+let rerenderEntireTree = (state) => {
+    ReactDOM.render( <App state = {state} 
+                    addPost = {addPost} 
+                    updateNewPost = {updateNewPost}/>, 
+                    document.getElementById('root') );
+}
 
+rerenderEntireTree(state);
+subscibe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+
+
+
+
+
