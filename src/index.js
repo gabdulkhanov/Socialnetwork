d7b6c282ece9401d701363612dcf2c74776f2629
gabdulkhanov <gabdulkhanov.lenar@mail.ru>
2020-07-18 21:47:21 +0500

    lesson 37 OOP

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 import './index.css';
 import App from './App';
-import state, { subscibe, addPost, updateNewPost } from './redux/state';
+import store from './redux/state';
 
 
 let rerenderEntireTree = (state) => {
-    ReactDOM.render( <App state = {state} 
-                    addPost = {addPost} 
-                    updateNewPost = {updateNewPost}/>, 
+    ReactDOM.render( <App state = {store.getState()} 
+                    addPost = {store.addPost.bind(store)} 
+                    updateNewPost = {store.updateNewPost.bind(store)}/>, 
                     document.getElementById('root') );
 }
 
-rerenderEntireTree(state);
-subscibe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscibe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,50 +1,64 @@
-import { rerenderEntire } from "../render";
 
-let state = {
-    profilePage : { 
-        posts :  [
-            { id: 1, message: 'Hello name Jon', age : 26 ,  likesCounts : 43 },
-            { id: 2, message: 'It\'s my first.', age : 24 , likesCounts : 1 },
-            { id: 3, message: 'Speak English', age : 871 , likesCounts : 9999 }
-        ],
-        newPostText: "qwerty" 
+let store = {
+
+    _state : {
+        profilePage : { 
+            posts :  [
+                { id: 1, message: 'Hello name Jon', age : 26 ,  likesCounts : 43 },
+                { id: 2, message: 'It\'s my first.', age : 24 , likesCounts : 1 },
+                { id: 3, message: 'Speak English', age : 871 , likesCounts : 9999 }
+            ],
+            newPostText: "qwerty" 
+        },
+
+        messagePage : {
+            dialogs : [
+                { id: 1, name: 'Bob' },
+                { id: 2, name: 'Mark' },
+                { id: 3, name: 'Sveta' },
+                { id: 4, name: 'Julia' },
+                { id: 5, name: 'Fred' }
+            ],
+
+            messages : [
+                { id: 1, message: 'Возможно.', likesCounts : 43 },
+                { id: 2, message: 'Do you speak English?.', likesCounts : 0 },
+                { id: 3, message: 'Thanks!' , likesCounts : 67 },
+                { id: 4, message: 'Yo Yo Yo' , likesCounts : 75},
+                { id: 5, message: 'Some message' , likesCounts : 3 }
+            ]
+        }
     },
 
-    messagePage : {
-        dialogs : [
-            { id: 1, name: 'Bob' },
-            { id: 2, name: 'Mark' },
-            { id: 3, name: 'Sveta' },
-            { id: 4, name: 'Julia' },
-            { id: 5, name: 'Fred' }
-        ],
+    getState() {
+        return this._state;
+    },
 
-        messages : [
-            { id: 1, message: 'Возможно.', likesCounts : 43 },
-            { id: 2, message: 'Do you speak English?.', likesCounts : 0 },
-            { id: 3, message: 'Thanks!' , likesCounts : 67 },
-            { id: 4, message: 'Yo Yo Yo' , likesCounts : 75},
-            { id: 5, message: 'Some message' , likesCounts : 3 }
-        ]
+    _rerenderEntireTree() {
+        alert("Tree");
+    },
+
+    addPost() {           
+        let newPost = {
+            id: 6,
+            message: this._state.profilePage.newPostText,
+            age: 27,
+            likesCounts: 33
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._rerenderEntireTree(this._state)
+    },
+
+    updateNewPost(newText) {    
+        this._state.profilePage.newPostText = newText;
+        this._rerenderEntireTree(this._state);
+    },
+
+    subscibe(observer)  {
+        this._rerenderEntireTree = observer;
     }
 }
 
-export let addPost = () => {    
-    let newPost = {
-        id: 6,
-        message: state.profilePage.newPostText,
-        age: 27,
-        likesCounts: 33
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntire(state)
-}
-
-export let updateNewPost = (newText) => {    
-    state.profilePage.newPostText = newText;
-    rerenderEntire(state)
-}
-
-export default state;
+export default store;
 
